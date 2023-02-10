7c5983d258cea5b67dda224d068cb05e0459502a
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-10 15:28:07 +0100

    changes

--- JS/codereview2.js
+++ JS/codereview2.js
@@ -17,36 +17,31 @@ let week = [{
 }, {
     taskName: "Cinema",
     description: "Select the movie for the weekly cinema evening",
-    importance: 4,
+    importance: 0,
     pic: "https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645__340.jpg"
 }, {
-    taskName: "football",
-    lName: "Go to the training",
-    description: "55",
+    taskName: "Football",
+    description: "Go to the training",
     importance: 5,
     pic: "https://cdn.pixabay.com/photo/2016/09/18/20/47/football-1678992__340.jpg"
 }, {
     taskName: "Partytime",
-    lName: "Select the location to meet your folks",
-    description: "42",
+    description: "Select the location to meet your folks",
     importance: 6,
     pic: "https://media.istockphoto.com/id/684007168/de/foto/drei-m%C3%A4nnlichen-freunden-karten-spielen-und-lachen-zu-hause.jpg?b=1&s=170667a&w=0&k=20&c=3kp6bE3zxlECSewbgn0Y-Suc_8AHuKFH8mkjvzFSwk8="
 }, {
     taskName: "Familytime",
-    lName: "Go to the grandparents and look after them",
-    description: "42",
-    importance: 7,
+    description: "Go to the grandparents and look after them",
+    importance: 0,
     pic: "https://media.istockphoto.com/id/144965031/de/foto/familientreffen.jpg?b=1&s=170667a&w=0&k=20&c=lDKfZi1uBqOv8a4kri-s_RbLAHF9bbDq6GF0tDDPCrg="
 }, {
     taskName: "Coding",
-    lName: "Learn for the Codereview",
-    description: "42",
+    description: "Learn for the Codereview",
     importance: 8,
     pic: "https://cdn.pixabay.com/photo/2017/06/29/01/02/home-office-2452806__340.jpg"
 }, {
     taskName: "Fitness",
-    lName: "Go to the Gym",
-    description: "42",
+    description: "Go to the Gym",
     importance: 9,
     pic: "https://cdn.pixabay.com/photo/2017/07/02/19/24/dumbbells-2465478__340.jpg"
 }];
@@ -58,10 +53,35 @@ for (let val of week) {
     <img src="${val.pic}" class="card-img-top" alt="${val.taskName}">
     <div class="card-body">
       <h5 class="card-title">${val.taskName}</h5>
-      <p class="${val.taskName}.</p>
-      <a href="#" class="btn btn-primary"><i class="bi bi-arrow-right-square-fill"></i> ${val.importance}</a>
-      <a href="#" class="btn btn-danger importanceBtn">Importance</a>
+      <p> ${val.description} </p>
+      <hr>
+      
+      <p> <i class="bi bi-arrow-right-square-fill"></i> <a href="#" class="btn btn-success importanceBtn">Priority Level </a><p class="increase">${val.importance}</p>
+      
     </div>
     </div>
     </div>`;
+}
+
+let btns = document.getElementsByClassName("importanceBtn");
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        week[i].importance++;
+        document.getElementsByClassName("increase")[i].innerHTML = week[i].importance;
+    })
+
+}
+
+function printPrio() {
+    let importance = document.getElementsByClassName("numbers").value;
+
+    document.getElementById("color").innerHTML = "<span id='color'>" + importance + "</span> ";
+
+
+    if (importance >= 1) {
+        document.getElementById("color").style.backgroundColor = "green";
+    } else {
+        document.getElementById("color").style.backgroundColor = "red";
+    }
 }
